7392cb8b1a73bd6a9e34abec80ca2b434784c599
EliverLara <eliverlara@gmail.com>
2019-07-03 13:01:02 -0500

    Improve gtk2 scrollbars style

--- gtk-2.0/render-assets.sh
+++ gtk-2.0/render-assets.sh
@@ -7,8 +7,12 @@ SRC_FILE="assets.svg"
 ASSETS_DIR="assets"
 INDEX="assets.txt"
 
+
 for i in `cat $INDEX`
 do 
+if [ -f $ASSETS_DIR/$i.png ]; then
+    echo $ASSETS_DIR/$i.png exists.
+else
     echo
     echo Rendering $ASSETS_DIR/$i.png
     $INKSCAPE --export-id=$i \
@@ -16,5 +20,6 @@ do
               --export-background-opacity=0 \
               --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
     && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
+fi
 done
 exit 0
