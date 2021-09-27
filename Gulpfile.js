187f6e1fcdd4bd04c0feba552328b5df71d5710a
EliverLara <eliverlara@gmail.com>
2021-09-27 15:35:08 -0500

    Update dependencies

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var exec = require('gulp-exec');
 
 gulp.task('styles', function(done) {
